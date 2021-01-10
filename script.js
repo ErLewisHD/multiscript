bbe68ee7f1adfecc443dec30bb214393df08f74d
ErLewisHD <73249413+ErLewisHD@users.noreply.github.com>
2021-01-10 20:32:19 +0100

    creacion script

---
+++ script.js
@@ -0,0 +1,42 @@
+preguntas = document.getElementsByClassName("qtext")
+opciones = document.getElementsByClassName("answer")
+bbdd = {
+    'En el formato de instrucciones tipo I de MIPS, el inmediato está indicado en:' :[
+        'En los bits del 31 al 16. '],
+    'Las instrucciones tipo I en el ISA del MIPS:'	:[
+        'Incluyen las instrucciones de salto condicional. ',
+        'Incluyen las cargas y almacenamientos. ',
+        'Incluyen las instrucciones de bifurcación a registro, bifurcación y enlace a registro. ',
+        'Pueden involucrar a un registro. ',
+        'Son instrucciones con inmediato. '],
+   'En secuencias de vídeo, hay una significativa correlación entre muestras pertenecientes a cuadros vecinos. Esta se denomina' :[
+	    'redundancia temporal ']
+
+
+}
+
+
+setTimeout(() => {
+    pregunta = []
+    for(let i = 0; i < preguntas.length; i++) {
+        pregunta[i] = preguntas.item(i).textContent
+    }
+
+    let i = 0
+    for(let j = 0; j < pregunta.length; j++) {
+        Object.entries(bbdd).forEach(([key, value]) => {
+            if (pregunta[i].includes(key)) {
+                opciones_test = opciones.item(i).childNodes
+                for (let j=0; j < (opciones_test.length); j+=2) {
+                    respuesta = opciones_test.item(j).textContent.slice(3)
+                    if (value.includes(respuesta)) {
+
+						 opciones_test.item(j).childNodes[1].childNodes[0].setAttribute("style","color:#504D4D")
+                        i ++
+                    }
+                }
+            }
+        });
+        i ++
+    }
+})
